535c3c49d805008e7a3f5594869aae9127ed7ff9
Richard Huang <flyerhzm@gmail.com>
2012-02-27 23:48:38 -0800

    add deploy capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application,       'huangzhimin.com'
+set :repository,        '_site'
+set :scm,               :none
+set :deploy_via,        :copy
+set :copy_compression,  :zip
+set :use_sudo,          false
+set :host,              'huangzhimin.com'
+set :keep_releases,     5
+
+role :web,  host
+role :app,  host
+role :db,   host, :primary => true
+
+set :user,    'huangzhi'
+set :group,   user
+
+set(:dest) { Capistrano::CLI.ui.ask("Destination: ") }
+set :deploy_to, '/home/huangzhi/sites/huangzhimin.com/production'
+
+before 'deploy:update', 'deploy:update_jekyll'
+
+namespace :deploy do
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    %x(rm -rf _site/* && bundle exec ejekyll)
+  end
+end
