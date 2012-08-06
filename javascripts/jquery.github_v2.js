1772d44aff68913d54c0afedc66fa11f0dd7db5f
Richard Huang <flyerhzm@gmail.com>
2012-08-06 23:06:16 +0800

    remove unused js

--- javascripts/jquery.github_v2.js
+++
@@ -1,5 +0,0 @@
-jQuery.githubUser = function(usernames, callback) {
-  $.each(usernames, function(index, username) {
-    jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
-  });
-}
